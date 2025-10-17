68356434e5d65a53b36990cbef84aa118c3ca06c
Kaden Nash <kadenrnash05@gmail.com>
2025-10-17 11:47:09 -0400

    Split kEvent into its own library of functions.

---
+++ src/Keyboard/Keyboard.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <windows.h>
+#include <vector>
+
+namespace Keyboard {
+    using std::vector;
+    
+    void kEvent(vector<WORD> input);
+    INPUT kEventPressHelper(INPUT inputStruct, WORD input);
+    INPUT kEventUnpressHelper(INPUT inputStruct, WORD input);
+    int getInputError(UINT sent, int sizeInput); 
+}
